8451b91e0351755634431d081c63c31e3787534b
kriscodesign <kriscodesign@gmail.com>
2025-06-06 22:40:18 +0100

    list task

---
+++ explore_datetime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timedelta, date
+
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f'Current date and time: {current_date}')
+
+def calculate_future_date():
+    user_days = int(input('Enter the number of days to add to the current date: '))
+    current_date = date.today()
+    future_date = current_date + timedelta(days=user_days)
+    print(f'Futre date: {future_date}')
+
+display_current_datetime()
+calculate_future_date()
