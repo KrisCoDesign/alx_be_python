83e59e6ca8c77af858b53f0e831be5e3c5eed543
kriscodesign <kriscodesign@gmail.com>
2025-06-13 20:35:40 +0100

    try-except

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,10 @@
+
+def safe_divide(numerator, denominator):
+    try:
+        result = float(numerator) / float(denominator)
+        return f"The result of the division is {result}"
+    except ZeroDivisionError:
+        print(f"Error: Cannot divide by zero.")
+    except ValueError:
+        print(f"Error: Please enter numeric values only.")
+   
