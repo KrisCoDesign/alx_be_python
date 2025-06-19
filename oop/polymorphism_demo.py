86871642ab19e1383bd5f3fc48d605a19938be43
kriscodesign <kriscodesign@gmail.com>
2025-06-19 19:31:00 +0100

    more on oop

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+
+class Shape:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        raise NotImplementedError("Area has not been calculated")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__(length, width)
+
+    def area(self):
+        return self.length * self.width
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius
+    
